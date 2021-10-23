3dd2961c8c792635bf45c4b6a8658c5390a8458e
David Plebanski <dplebanski@gmail.com>
2021-10-22 20:56:10 -0400

    commit before pull

--- src/main/java/com/techelevator/model/JDBCReservationDAO.java
+++ src/main/java/com/techelevator/model/JDBCReservationDAO.java
@@ -39,7 +39,6 @@ public class JDBCReservationDAO implements ReservationDAO{
 
 
             LocalDate startDate = reservation.getStartDate();
-            String startDateString = startDate.toString();
             LocalDate endDate = reservation.getEndDate();
             LocalDate userStartDate = LocalDate.of(2021, 10, 19);
 
